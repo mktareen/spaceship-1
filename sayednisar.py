544970a4a5c2110f126c5ee3fc7bb57f1e6031cd
nisarsadaat <nisarsadaat@gmail.com>
2022-06-08 00:14:11 +0430

    Create sayednisar.py

---
+++ sayednisar.py
@@ -0,0 +1,33 @@
+
+
+def pyramid():
+  for i in range(1,5):
+   print(' ',end = '')
+   if i==4:
+    print('*')
+    
+
+  for i in range(1,4):
+   print(' ',end ='')
+   if i==3:
+    print('*' * 3)
+
+
+  for i in range(1,3):
+   print(' ',end ='')
+   if i==2:
+    print('*' * 5) 
+    
+
+  for i in range(1,2):
+   print(' ',end ='')
+   if i==1:
+    print('*' * 7)
+    
+
+    
+    
+pyramid()    
+pyramid() 
+
+pyramid() 
